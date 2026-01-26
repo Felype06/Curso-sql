cd962141be4a08ddfd0774f0a6b14027fefdbf56
Felype06 <felype.fdd@hotmail.com>
2026-01-26 13:53:20 -0300

    Configurando arquivos ignorados e limpando cache

--- bora_praticar/01_where.sql
+++
@@ -1,7 +0,0 @@
--- Selecione todos o clientes com email cadsatrados
-
-SELECT *
-
-FROM clientes
-
-WHERE flEmail = 1
--- bora_praticar/03_where.sql
+++
@@ -1,7 +0,0 @@
--- Selecione todos cliemtes com mais de 500 pontos
-
-SELECT IdCliente, qtdePontos
-
-FROM clientes
-
-WHERE qtdePontos >= 500
